4a472e27a755610e67f6c9dc5605a4f8a3a7f97d
Thays Casado <thayscasado@hotmail.com>
2021-08-24 00:55:53 -0700

    added a check useState

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 function App() {
-  // the useState will return two fields
-  // 0: undefined 1: a function
-  // const result = useState();
-
-  const [status, setStatus] = useState("Not Delivered");
+  const [checked, setChecked] = useState(false);
 
   return (
     <div>
-      <h1>The package is: {status}.</h1>
-      <button onClick={() => setStatus("Delivered")}>Deliver</button>
+      <input
+        type="checkbox"
+        value={checked}
+        onChange={() => setChecked((checked) => !checked)}
+      />
+      <p>{checked ? "checked" : "not checked"}</p>
     </div>
   );
 }
