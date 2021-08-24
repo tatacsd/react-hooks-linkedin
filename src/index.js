868b10f1716479c337541b90ef1c9b899266e6e3
Thays Casado <thayscasado@hotmail.com>
2021-08-24 01:51:36 -0700

    use effect chnaging only the changed fields

--- src/index.js
+++ src/index.js
@@ -4,18 +4,35 @@ import "./index.css";
 
 function App() {
   const [name, setName] = useState("Will");
+  const [admin, setAdmin] = useState(false);
 
   // useEffect takes a function and
   // it runs after each update
+  useEffect(
+    () => {
+      // change the document title
+      document.title = `Celebrate ${name}`;
+      console.log(`Celebrate ${name}`);
+    },
+    // to set it to run only once
+    //or when the variable is changed
+    [name]
+  );
+
   useEffect(() => {
-    // change the document title
-    document.title = `Celebrate ${name}`;
-  });
+    console.log(`The user is: ${admin ? "Admin" : "not admin"}`);
+  }, [admin]);
 
   return (
     <div>
       <h1>Congratulations {name}!</h1>
-      <button onClick={() => setName("John")}>Change Winner</button>
+      <button
+        onClick={() => setName((name) => (name === "Will" ? "John" : "Will"))}
+      >
+        Change Winner
+      </button>
+      <p>{admin ? "logged in" : "not logged in"}</p>
+      <button onClick={() => setAdmin((admin) => !admin)}>Log in</button>
     </div>
   );
 }
